2daac8dbfc046e25d6e294b8c9cb710c8cbc79e9
Donato Perconti <donato.perconti@gmail.com>
2017-11-29 14:08:00 -0800

    WIP:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ComponentNameComponent } from './component-name/component-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ComponentNameComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
